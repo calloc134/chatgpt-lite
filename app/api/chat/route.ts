99d58796eb2ea04345786797f1ab6f1751b54309
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-06-28 18:38:42 +0900

    不要コード削除

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,8 +10,7 @@ export interface Message {
 }
 
 const openaiClient = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  baseURL: process.env.OPENAI_API_BASE_URL // 未設定なら自動で api.openai.com
+  apiKey: process.env.OPENAI_API_KEY
 })
 
 export async function POST(req: NextRequest) {
@@ -75,110 +74,6 @@ const getResponseStream = async (model: string, messages: Message[]) => {
         }
       } catch (err) {
         controller.error(err)
-      }
-    }
-  })
-}
-
-const getApiConfig = () => {
-  const useAzureOpenAI =
-    process.env.AZURE_OPENAI_API_BASE_URL && process.env.AZURE_OPENAI_API_BASE_URL.length > 0
-
-  let apiUrl: string
-  let apiKey: string
-  let model: string
-  if (useAzureOpenAI) {
-    let apiBaseUrl = process.env.AZURE_OPENAI_API_BASE_URL
-    const apiVersion = '2024-02-01'
-    const deployment = process.env.AZURE_OPENAI_DEPLOYMENT || ''
-    if (apiBaseUrl && apiBaseUrl.endsWith('/')) {
-      apiBaseUrl = apiBaseUrl.slice(0, -1)
-    }
-    apiUrl = `${apiBaseUrl}/openai/deployments/${deployment}/chat/completions?api-version=${apiVersion}`
-    apiKey = process.env.AZURE_OPENAI_API_KEY || ''
-    model = '' // Azure Open AI always ignores the model and decides based on the deployment name passed through.
-  } else {
-    let apiBaseUrl = process.env.OPENAI_API_BASE_URL || 'https://api.openai.com'
-    if (apiBaseUrl && apiBaseUrl.endsWith('/')) {
-      apiBaseUrl = apiBaseUrl.slice(0, -1)
-    }
-    apiUrl = `${apiBaseUrl}/v1/chat/completions`
-    apiKey = process.env.OPENAI_API_KEY || ''
-    model = process.env.OPENAI_MODEL || 'gpt-3.5-turbo'
-  }
-
-  return { apiUrl, apiKey, model }
-}
-
-const getOpenAIStream = async (
-  apiUrl: string,
-  apiKey: string,
-  model: string,
-  messages: Message[]
-) => {
-  const encoder = new TextEncoder()
-  const decoder = new TextDecoder()
-  const res = await fetch(apiUrl, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-      'api-key': `${apiKey}`
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      model: model,
-      frequency_penalty: 0,
-      max_tokens: 4000,
-      messages: messages,
-      presence_penalty: 0,
-      stream: true,
-      temperature: 0.5,
-      top_p: 0.95
-    })
-  })
-
-  if (res.status !== 200) {
-    const statusText = res.statusText
-    const responseBody = await res.text()
-    console.error(`OpenAI API response error: ${responseBody}`)
-    throw new Error(
-      `The OpenAI API has encountered an error with a status code of ${res.status} ${statusText}: ${responseBody}`
-    )
-  }
-
-  return new ReadableStream({
-    async start(controller) {
-      const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data
-
-          if (data === '[DONE]') {
-            controller.close()
-            return
-          }
-
-          try {
-            const json = JSON.parse(data)
-            const text = json.choices[0]?.delta?.content
-            if (text !== undefined) {
-              const queue = encoder.encode(text)
-              controller.enqueue(queue)
-            } else {
-              console.error('Received undefined content:', json)
-            }
-          } catch (e) {
-            console.error('Error parsing event data:', e)
-            controller.error(e)
-          }
-        }
-      }
-
-      const parser = createParser(onParse)
-
-      for await (const chunk of res.body as any) {
-        // An extra newline is required to make AzureOpenAI work.
-        const str = decoder.decode(chunk).replace('[DONE]\n', '[DONE]\n\n')
-        parser.feed(str)
       }
     }
   })
